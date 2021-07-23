bab1ad3e7d5b146b67761fb6137017d9111819f6
Tessa Azar MacLean <tessaamac@gmail.com>
2021-07-23 22:37:32 +0000

    Separate createTables and seedTables

--- express-backend/bin/resetdb.js
+++ express-backend/bin/resetdb.js
@@ -13,32 +13,43 @@ const connection = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
-// Create and seed database with test data
+// Create database tables
 const createTables = () => {
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log('You are now connected...');
+  try {
+    connection.query('DROP TABLE IF EXISTS users;');
+    connection.query('DROP TABLE IF EXISTS expenses;');
+    connection.query('CREATE TABLE users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255))');
+    connection.query('CREATE TABLE expenses (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL, cost INT NOT NULL, category VARCHAR(255) NOT NULL, timestamp TIMESTAMP NOT NULL DEFAULT NOW())');
+  } catch (err) {
+    return console.log("ERROR: ", err);
+  }
 
-    try {
-      connection.query('DROP TABLE IF EXISTS users;');
-      connection.query('DROP TABLE IF EXISTS expenses;');
-      connection.query('CREATE TABLE users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255))');
-      connection.query('CREATE TABLE expenses (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL, cost INT NOT NULL, category VARCHAR(255) NOT NULL, timestamp TIMESTAMP NOT NULL DEFAULT NOW())');
-      connection.query('INSERT INTO users (email, password, name) VALUES (?, ?, ?)', ['example@example.com', 'password', 'Fake User']);
-      connection.query('INSERT INTO expenses (name, cost, category) VALUES (?, ?, ?)', ['Electricity', 300, 'Bills']);
-      connection.query('SELECT * FROM users', function(err, results) {
-        if (err) throw err;
-        console.log("users table", results);
-      });
-      connection.query('SELECT * FROM expenses', function(err, results) {
-        if (err) throw err;
-        console.log("expenses table", results);
-      });
-    } catch (err) {
-      return console.log("ERROR: ", err);
-    } finally {
-      connection.end();
-    }
-  });
 };
-createTables();
+
+// Seed with test account(s) and expense(s)
+const seedTables = () => {
+  try {
+    connection.query('INSERT INTO users (email, password, name) VALUES (?, ?, ?)', ['example@example.com', 'password', 'Fake User']);
+    connection.query('INSERT INTO expenses (name, cost, category) VALUES (?, ?, ?)', ['Electricity', 300, 'Bills']);
+    connection.query('SELECT * FROM users', function(err, results) {
+      if (err) throw err;
+      console.log("users table", results);
+    });
+    connection.query('SELECT * FROM expenses', function(err, results) {
+      if (err) throw err;
+      console.log("expenses table", results);
+    });
+  } catch (err) {
+    return console.log("ERROR: ", err);
+  }
+};
+
+// Create connection and reset database
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Successful connection!');
+  createTables();
+  // Comment out 'seedTables();' once in production
+  seedTables();
+  connection.end();
+});
