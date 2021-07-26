7ebe0e0e002361da597fa108579025e0127dfb95
Tessa Azar MacLean <tessaamac@gmail.com>
2021-07-26 20:17:50 +0000

    Add header

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -8,6 +8,9 @@ const App = () => {
 
   return (
     <div className="App">
+        <header>
+          Expenses tracker! Keep track of what you're spending on :)
+        </header>
         <Form />
         <Table />
     </div>
